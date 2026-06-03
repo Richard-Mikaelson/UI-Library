7d5ba6934e1382d93938899d85635ccf938c8965
Richard-Mikaelson <192161388+Richard-Mikaelson@users.noreply.github.com>
2026-06-03 15:02:05 +0200

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -579,7 +579,7 @@ function IceHub.CreateMain(gameName, subtitle)
     ServerButtonsFrame.BackgroundTransparency = 0.5
     ServerButtonsFrame.BorderColor3 = Color3.fromRGB(20, 20, 20)
     ServerButtonsFrame.Size = UDim2.new(0, 285, 0, 50)
-    ServerButtonsFrame.LayoutOrder = 2
+    ServerButtonsFrame.LayoutOrder = 0
     
     ServerButtonsCorner.CornerRadius = UDim.new(0, 4)
     ServerButtonsCorner.Parent = ServerButtonsFrame
